31f94dd61b52ddab455ebce4df08eb5e97a29223
Archit Srivastava <apsrivastava0909@gmail.com>
2019-01-06 12:39:00 +0530

    Data is successfully saving in DB

---
+++ DAO/SendData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DBConnection;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Entities;
+
+namespace DAO
+{
+    public class SendData
+    {
+        public SendData()
+        { }
+
+        public string CallDBToSend(DBConnectionTesting data)
+        {
+            try
+            {
+                DBConnectionClass dBConnection = new DBConnectionClass
+                {
+                    DatabaseName = "TestDB"
+                };
+
+                string Col_FirstName = data.FirstName;
+                string Col_LastName = data.LastName;
+
+                if (dBConnection.IsConnect())
+                {
+                    string query = "INSERT INTO TestTable (Column1,Column2) values (@Col_FirstName , @Col_LastName)";
+                    var cmd = new SqlCommand(query, dBConnection.Connection);
+                    cmd.Parameters.AddWithValue("@Col_FirstName", Col_FirstName);
+                    cmd.Parameters.AddWithValue("@Col_LastName", Col_LastName);
+                    string value = cmd.ExecuteNonQuery().ToString();
+                    
+                    dBConnection.Close();
+                    return value;
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("Conn Failed");
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("Unsuccessful Query Excution");
+                throw e;
+            }
+        }
+    }
+}
